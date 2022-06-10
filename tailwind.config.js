bde64d2a1c4b2738f8fe1892cf5d34a0640ed848
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-06-09 22:25:43 -0300

    feat: add style guide in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          900: '#1F1F1F',
+        },
+        gray: {
+          100: '#E5E5E5',
+          300: '#ABAAB2',
+          500: '#9695AC',
+          800: '#828290',
+        },
+        rose: {
+          300: '#D3828F',
+          500: '#B76E79',
+        },
+      },
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      'abril-fatface': ['Abril Fatface', 'cursive'],
+      mulish: ['Mulish', 'sans-serif'],
+    },
   },
   plugins: [],
 };
