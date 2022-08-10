14dc0f0cec6b5eb7f9a8b6cfff42c78b67133bb7
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-08-09 22:36:08 -0300

    feat: add metas and favicon to page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../assets/global.scss';
 
 export default function NextApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Giovanna Ramaldo</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portfólio Giovanna Ramaldo"></meta>
+      </Head>
       <Component {...pageProps} />
     </>
   );
