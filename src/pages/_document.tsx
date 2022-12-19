9e2d82f96f83df5ce2c5b10a79dff637458810ce
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-12-18 23:20:20 -0300

    feat: import fonts in scss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { Fonts } from 'components/Head/Fonts';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default class NextDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <Fonts />
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
